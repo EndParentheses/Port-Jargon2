7b968bb56aa090946d34d58bd135e74802f1e799
EndParentheses <31145147+EndParentheses@users.noreply.github.com>
2017-08-18 18:54:38 -0400

    Add files via upload

---
+++ JavaSample.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+  <header>My name is</header>
+</head>
+<body>
+<script type="text/javascript">
+  document.write("Hello, everybody. The answer to the riddle posed by the header is LBJ");
+  
+</script>
+
+</body>
+</html>
